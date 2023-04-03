2acb33f7a8c1b17bed1362b3d097c326efbe0a04
adoalex <64977187+adoalex5@users.noreply.github.com>
2023-04-03 11:50:38 -0900

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/**
- *print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- *followed by a new line
- */
 
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
 void print_alphabet_x10(void)
 {
 	char ch;
@@ -17,10 +17,28 @@ void print_alphabet_x10(void)
 		while (ch <= 'z')
 		{
 			_putchar(ch);
-		        ch++;
+			ch++;
 		}
 		_putchar('\n');
 		i++;
 	}
 }
 
+
+3-islower.c
+
+#include "main.h"
+
+/**
+ * _islower - check if char is lowercase
+ * @c: is the char to be checked
+ * Return: 1 if char is lowercase, otherwise 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
